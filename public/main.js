d380646f3837dcb76ac88d69547198e6786bc426
Yukihiko Kikuta <daleyuki@gmail.com>
2026-05-13 17:25:24 +0900

    v2.22 deepen poem placement

--- public/main.js
+++ public/main.js
@@ -558,7 +558,7 @@ const createCurrentTanzakuCanvas = async () => {
   const photoW = 390;
   const photoH = 488;
   const jpX = 835;
-  const jpY = 550;
+  const jpY = 800;
   const jpFontSize = 70;
   const jpColumnGap = 100;
   const jpLetterGap = 69;
